1335a0c97deef4b017cf3b0199d8bad297e019c9
rebeccanoren <hello@rebeccanoren.com>
2022-09-25 18:51:57 +0200

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,18 @@
 <script>
-	import Counter from '$lib/Counter.svelte';
+  let illustration = 'resources/illustration.svg';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Templateas</title>
+	<meta name="description" content="Templateas" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
+<section class="work">
+	<img alt="Illustration of a person building" src={ illustration}/>
+	<h1>We are building this site
 	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h2>Promise to come back soon for more 💖</h2>	
+	<a href="https://www.etsy.com/shop/Templateas"><button>I'll check out the Etsy shop</button></a>
 </section>
 
 <style>
@@ -35,23 +24,4 @@
 		flex: 1;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
